e32a22cee632a3847837848dd4cc2edc11db40d4
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-20 15:08:42 -0700

    Created Home component

---
+++ covid-19/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+function Home() {
+    return (
+        <div>
+            <h3>Home Component</h3>
+        </div>
+    )
+}
+
+export default Home;
