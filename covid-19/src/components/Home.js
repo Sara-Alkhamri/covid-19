bfb6885a9f0cddab52ffba426a1d43adb14ac8bb
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-20 15:46:50 -0700

    Added axios call to get all data in Home.js

--- covid-19/src/components/Home.js
+++ covid-19/src/components/Home.js
@@ -2,9 +2,25 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Home() {
+    const [data, setData] = useState([])
+
+    const getData = () => {
+        axios
+        .get('https://api.covid19api.com/all')
+        .then(res => {
+            console.log(res.data)
+            setData(res.data)
+        })
+        .catch(err => console.log(err.res));
+    }
+    useEffect(() => {
+        getData();
+      }, []);
+
     return (
         <div className="container">
             <h3>Home Component</h3>
+            <div>data={data}</div>
         </div>
     )
 }
